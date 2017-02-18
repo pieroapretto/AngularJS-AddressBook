b9266d23ef41b2db8b8bff3e69a3a9bacf09fb1c
ASTPMeetup <pretto417@gmail.com>
2017-02-18 08:52:43 -0800

    attached to local js/css libraries

---
+++ js/style.js
@@ -0,0 +1,8 @@
+$('.dropdown').on('show.bs.dropdown', function() {
+  $(this).find('.dropdown-menu').first().stop(true, true).slideDown();
+});
+
+// Add slideUp animation to Bootstrap dropdown when collapsing.
+$('.dropdown').on('hide.bs.dropdown', function() {
+  $(this).find('.dropdown-menu').first().stop(true, true).slideUp();
+});
